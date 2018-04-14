02087467ac2a70795bcd14a0430d428fc4b23d0c
Ndate <38379682+Ndate@users.noreply.github.com>
2018-04-14 16:49:19 +0200

    Add files via upload

---
+++ SPI_2553.h
@@ -0,0 +1,15 @@
+/*
+ * SPI_2553.h
+ *
+ *  Created on: 25 mars 2018
+ *      Author: LENOVO
+ */
+
+#ifndef SPI_2553_H_
+#define SPI_2553_H_
+
+void Send_char_SPI( unsigned char );
+
+void init_USCI( void );
+
+#endif /* SPI_2553_H_ */
